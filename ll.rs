d1a2915aad4317c8f71ab610de13e065e2a31cea
Brian Anderson <banderson@mozilla.com>
2012-10-17 15:53:56 -0700

    Add definitions of a few data structures that rust-netsurfcss needs

--- ll.rs
+++ ll.rs
@@ -1,7 +1,18 @@
-use core::libc::{c_char, c_void};
+use core::libc::{c_char, c_void, size_t};
 use core::libc::types::common::c99::uint32_t;
 
 #[nolink]
 #[link_args="-L../libwapcaplet -lwapcaplet"]
 pub extern mod linking { }
 
+pub type lwc_refcounter = uint32_t;
+pub type lwc_hash = uint32_t;
+
+pub struct lwc_string {
+    prevptr: **lwc_string,
+    next: *lwc_string,
+    len: size_t,
+    hash: lwc_hash,
+    refcnt: lwc_refcounter,
+    insensitive: *lwc_string
+}
